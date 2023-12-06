5e855bf696d413a64f9fb5755cae19d32c0638f8
Sagar <dev.sagartmg@gmail.com>
2023-12-06 17:03:53 +0545

    props in react

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -3,33 +3,64 @@ import ReactDOM from 'react-dom/client'
 
 import "./main.css"
 
-/* Card Component */
-function Card() {
-  return <div class='card'>
-    <p class="title">react</p>
-    <p>Rs:1200</p>
+/* 
+function Course(props) {   // object destructuring
+  console.log({props});
+  return <div class="card">
+    <p class="title">{props.courseTitle} </p>
+    <p>price:Rs.{props.coursePrice}</p>
+    <p>duration:{props.duration}</p>
     <hr />
-    react is Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, repellendus.
+    <p>Lorem ipsum dolor sit amet.</p>
+  </div>
+} 
+*/
+
+/* default import export in react */
+/* named import export in react */
+
+const Course = (props) => {  // props :{data:{course:{title:"react"}} }
+  console.log({ props })
+  return <div class="card">
+    <p class="title">{props.data?.course.title} </p>  {/* ?. optinal chaining */}
   </div>
 }
-
-Card("react", 1000)
-Card("node", 1200)
 
 
 let cards = ["react", "node", "git"]
 
+let reactData = {
+  course: {
+    title: "react"
+  }
+}
+
+let nodeData = {
+  course: {
+    title: "node"
+  }
+}
+
+
+
+const User = (props) => {
+  let { age, name, address } = props // object-destructuring.
+  return <div className='card'>
+    <p>{name} ({age})</p>
+    <p>{address}</p>
+  </div>
+
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-    <h1>React</h1>
-    <h2>Element</h2>
-    <h2>component</h2>
-    <h2>research on props in react   (parameter)</h2>
-    <h1>Total Cards {cards.length} </h1>
-    <Card title="react" price="1000" />  {/* not Card("react",1000) */}
-    <Card />
-    <Card />
+    <h1 >Our courses ({cards.length})  </h1>
+    <Course data={reactData} />
+    <Course data={nodeData} />
+    <Course />
+    <User name="ram" age={10} address="boudha" />
+    <User name="hari" age={10} address="tinkune" />
+    <User name="shyyam" age={10} address="banewshowr" />
 
   </React.StrictMode>,
 )
