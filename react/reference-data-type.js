3e8fd448689081fba5567cb8e56f2108c473fe92
Sagar <dev.sagartmg@gmail.com>
2023-12-18 17:07:59 +0545

    dynmaic todos using state

---
+++ react/reference-data-type.js
@@ -0,0 +1,13 @@
+let num1 = 100
+let num2 = num1 // 100
+num2 = 200
+
+console.log(num1);
+console.log(num2);
+
+/* referemce data types */
+let names = ["ram","shyam"]
+let otherNames = [...names] // othernames: ["ram","shyam
+otherNames.push("hari")
+console.log("names",names);
+console.log(otherNames);
